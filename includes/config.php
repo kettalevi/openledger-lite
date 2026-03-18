abeb723c0376943f4fb01fc904ecf66e9af13dc2
Martin Levi K.A <kettalevi@gmail.com>
2026-03-18 21:33:55 +0300

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'yourdbname');
-define('DB_USER', 'yourdbuser');
-define('DB_PASS', 'yourdbpassword');
+define('DB_NAME', 'openledger_lite');
+define('DB_USER', 'root');
+define('DB_PASS', '');
 
 session_start();
