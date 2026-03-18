40e31970539199f2f6e750e9f22ddad1edd999e2
Martin Levi K.A <kettalevi@gmail.com>
2026-03-18 21:23:38 +0300

    Add script to create a default admin user
    This script creates a default user with admin credentials in the database.

---
+++ includes/user.php
@@ -0,0 +1,9 @@
+<?php
+require '../db.php';
+//use this file to create a default user. Delete immediately after.
+$password = password_hash("admin123", PASSWORD_DEFAULT);
+
+$conn->prepare("INSERT INTO users (name, email, password) VALUES (?, ?, ?)")
+     ->execute(["Admin", "admin@test.com", $password]);
+
+echo "User created!";
